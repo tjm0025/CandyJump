a2e0ca3d155d294bd89adcac7232544dd42662d7
Tyanna Malone <tjm0025@auburn.edu>
2024-04-22 11:04:55 -0500

    Added backgrounds, created function to set background image

--- level.py
+++ level.py
@@ -17,13 +17,32 @@ Player_Vel = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def set_background(name):
+    image = pygame.image.load(join("assets", "Backgrounds", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i + width, j * height]
+            tiles.append(pos)
+    return tiles, image,
+
+
 
 def main(window):
     clock = pygame.time.Clock()
-
+    background = set_background("tile_0008.png")
     run = True
     while run:
         clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    pygame.quit()
+    quit()
 
 
 if __name__ == '__main__':
